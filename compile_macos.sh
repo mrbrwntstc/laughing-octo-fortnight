93a7f0cd5b11a399431578a56a634de43dfd0afb
Gaurav Bhaskar <geekman95@gmail.com>
2024-12-26 08:19:31 -0600

    initial commit

---
+++ compile_macos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PROJECT_DIR=$(pwd)
+BUILD_DIR="${PROJECT_DIR}/build"
+EXE_NAME="linear_algebra"
+
+SRC_FILES="${PROJECT_DIR}/main.cpp"
+
+# Step 2: Create the build directory
+if [ ! -d "$BUILD_DIR" ]; then
+    echo "Creating build directory..."
+    mkdir "$BUILD_DIR"
+fi
+
+clang++ -std=c++11 -o "$BUILD_DIR/$EXE_NAME" $SRC_FILES
